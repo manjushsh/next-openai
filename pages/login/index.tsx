5c72d71a265cb343ca4203c877b36b21ac5f1c2d
Manjush Shetty <manjush@workplacefabric.com>
2022-04-14 11:48:05 +0530

    check

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -12,7 +12,8 @@ const LogIn = ({ state, updateLogin }: any) => {
             headers,
             body,
         };
-        const finalURL = `http://${process?.env?.NEXT_PUBLIC_VERCEL_URL || 'localhost:3000'}/api/list-engines`;
+        // const finalURL = `http://${process?.env?.NEXT_PUBLIC_VERCEL_URL || 'localhost:3000'}/api/list-engines`;
+        const finalURL = `${window.location.protocol} + "//" + ${window.location.host}`
         console.warn('URL: ', finalURL);
         fetch(finalURL, requestData)
             .then(response => response.json())
