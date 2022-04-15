2f21e542eac97c9d8abe921911b04b8c70f241e9
manjushsh <manjushsh@protonmail.com>
2022-04-15 11:37:19 +0530

    css changes

--- pages/nxt-chat.tsx
+++ pages/nxt-chat.tsx
@@ -1,14 +1,13 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import styles from '../styles/Home.module.css';
 import Chat from './chat';
 
 const NextChat: NextPage = () => {
     const { query }: any = useRouter();
 
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>
+        <div>
+            <main>
                 <Chat OPENAI_API_KEY={query?.OPENAI_API_KEY} OPEN_AI_ORG={query?.OPEN_AI_ORG} />
             </main>
         </div>
